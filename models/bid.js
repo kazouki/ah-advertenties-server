d0b8f8070a63266ecda3fd0c7a632bdef8b808ff
kazouki <kazouki@hotmail.com>
2020-07-23 13:58:18 +0200

    setup basic model relations

--- models/bid.js
+++ models/bid.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class bid extends Model {
     /**
@@ -10,16 +8,19 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      bid.belongsTo(models.card);
     }
-  };
-  bid.init({
-    email: DataTypes.STRING,
-    amount: DataTypes.FLOAT,
-    cardId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'bid',
-  });
+  }
+  bid.init(
+    {
+      email: DataTypes.STRING,
+      amount: DataTypes.FLOAT,
+      cardId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "bid",
+    }
+  );
   return bid;
 };
