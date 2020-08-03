f6ecc95d0d39ef4e938f34890280773152d8adc7
kazouki <kazouki@hotmail.com>
2020-08-03 14:43:40 +0200

    edit deployment config

--- config/config.js
+++ config/config.js
@@ -14,10 +14,8 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    url: process.env.DATABASE_URL_DEV,
     dialect: "postgres",
+    operatorsAliases: "0",
   },
 };
